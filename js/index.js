4da1b6786ae9f69145f6ec185223a08ddc4d2438
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-06-17 18:17:35 -0400

    almost done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,62 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// const ctaH1 = document.createElement('h1');
+// ctaH1.textContent = "DOM is Awesome!";
+// const sectionCta = document.querySelector('.cta-text');
+// sectionCta.prepend(ctaH1);
+const ctaH1 = document.querySelector('h1');
+ctaH1.textContent = siteContent["cta"]["h1"];
+
+const bttn = document.querySelector("button");
+bttn.textContent = siteContent["cta"]["button"];
+
+let navBar = document.querySelector('nav').children;
+navBar[0].textContent = siteContent["nav"]["nav-item-1"];
+navBar[1].textContent = siteContent["nav"]["nav-item-2"];
+navBar[2].textContent = siteContent["nav"]["nav-item-3"];
+navBar[3].textContent = siteContent["nav"]["nav-item-4"];
+navBar[4].textContent = siteContent["nav"]["nav-item-5"];
+navBar[5].textContent = siteContent["nav"]["nav-item-6"];
+
+const header4 = document.querySelectorAll('h4');
+header4[0].textContent = siteContent["main-content"]["features-h4"];
+header4[1].textContent = siteContent["main-content"]["about-h4"];
+header4[2].textContent = siteContent["main-content"]["services-h4"];
+header4[3].textContent = siteContent["main-content"]["product-h4"];
+header4[4].textContent = siteContent["main-content"]["vision-h4"];
+header4[5].textContent = siteContent["contact"]["contact-h4"];
+
+const paraEl = document.querySelectorAll('p');
+paraEl[0].textContent = siteContent["main-content"]["features-content"];
+paraEl[1].textContent = siteContent["main-content"]["about-content"];
+paraEl[2].textContent = siteContent["main-content"]["services-content"];
+paraEl[3].textContent = siteContent["main-content"]["product-content"];
+paraEl[4].textContent = siteContent["main-content"]["vision-content"];
+paraEl[5].textContent = siteContent["contact"]["address"];
+paraEl[6].textContent = siteContent["contact"]["phone"];
+paraEl[7].textContent = siteContent["contact"]["email"];
+paraEl[8].textContent = siteContent["footer"]["copyright"];
+
+
+let newAtag = document.createElement('a');
+newAtag.textContent = "Hades";
+let newAtag2 = document.createElement('a');
+newAtag2.textContent = "Zeus";
+
+const newNavContent = document.querySelector('nav');
+newNavContent.appendChild(newAtag);
+newNavContent.appendChild(newAtag2);
+
+let navChange = document.querySelectorAll('a');
+navChange.forEach(function(currentValue){
+  currentValue.style.color = 'green';
+});
+
